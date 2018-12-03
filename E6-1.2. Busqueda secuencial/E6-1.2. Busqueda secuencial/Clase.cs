156caf701653d272eb3c293da265862d8c2c145b
CesarDSC2 <42946037+CesarDSC2@users.noreply.github.com>
2018-12-03 11:59:01 -0800

    Add files via upload

---
+++ E6-1.2. Busqueda secuencial/E6-1.2. Busqueda secuencial/Clase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E6_1._2.Busqueda_secuencial
+{
+    class Clase
+    {
+        public void Play()
+        {
+            
+            Console.Write("¿Cuántos alumnos són?: ");
+            int cant = Convert.ToInt32(Console.ReadLine());
+            int[] conjunto = new int[cant]; //Creamos un vector.
+            Console.Clear();
+            for (int j = 0; j < cant; j++)
+            {
+                Console.Write("Ingrese la calificación el alumno {0}: ", j+1); //Capturamos las calificaciones dentro del Vector.
+                conjunto[j] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("Búsqueda:");
+            int busqueda = Convert.ToInt32(Console.ReadLine());
+            var salida = conjunto.Where(con => con == busqueda); //Utilizamos una expresión "Lambda" para buscar el elemento.
+            int i = 1;
+            Console.WriteLine("~ ~ ~ ~ ~ ~ ~ ~");
+            foreach (var item in salida) //Con un foreach, el el elemento existe, lo despliega, si no, no imprime nada.
+            {
+                Console.WriteLine(i + ".-" + item.ToString());
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ E6-1.2. Busqueda secuencial/E6-1.2. Busqueda secuencial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E6_1._2.Busqueda_secuencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clase obj = new Clase(); //Instancia de la Clase.
+
+            obj.Play();
+        }
+    }
+}
